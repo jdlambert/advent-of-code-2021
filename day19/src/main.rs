257b8bc7496fb2ad42315d82fbbc1fae9b123bcd
Jacob Lambert <jacob.lambert@gmail.com>
2021-12-19 20:33:22 -0800

    Refactor day 19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
 type Point = (isize, isize, isize);
+type Fingerprint = (isize, isize);
+type Fingerprints = HashMap<Fingerprint, Vec<(Point, Point)>>;
 
 fn sub((x0, y0, z0): Point, (x1, y1, z1): Point) -> Point {
     (x0 - x1, y0 - y1, z0 - z1)
@@ -10,29 +12,34 @@ fn add((x0, y0, z0): Point, (x1, y1, z1): Point) -> Point {
     (x0 + x1, y0 + y1, z0 + z1)
 }
 
-fn norm(a: Point, b: Point) -> isize {
+fn l1_diff(a: Point, b: Point) -> isize {
     let (dx, dy, dz) = sub(a, b);
-    dx * dx + dy * dy + dz * dz
+    dx.abs() + dy.abs() + dz.abs()
 }
 
-fn part1(solution: &[(Point, Vec<Point>)]) -> usize {
-    solution
-        .iter()
-        .flat_map(|(_, beacons)| beacons)
-        .collect::<HashSet<_>>()
-        .len()
+fn lmax_diff(a: Point, b: Point) -> isize {
+    let (dx, dy, dz) = sub(a, b);
+    dx.abs().max(dy.abs()).max(dz.abs())
 }
 
-fn part2(solution: &[(Point, Vec<Point>)]) -> isize {
-    solution
+fn fingerprint(a: Point, b: Point) -> Fingerprint {
+    (l1_diff(a, b), lmax_diff(a, b))
+}
+
+fn part1((_, beacons): &(Vec<Point>, HashSet<Point>)) -> usize {
+    beacons.len()
+}
+
+fn part2((scanners, _): &(Vec<Point>, HashSet<Point>)) -> isize {
+    scanners
         .iter()
-        .flat_map(|(l, _)| solution.iter().map(move |(r, _)| (l, r)))
-        .map(|((x0, y0, z0), (x1, y1, z1))| (x0 - x1).abs() + (y0 - y1).abs() + (z0 - z1).abs())
+        .flat_map(|l| scanners.iter().map(move |r| (l, r)))
+        .map(|(&a, &b)| l1_diff(a, b))
         .max()
         .unwrap()
 }
 
-fn orient((x, y, z): Point, i: usize) -> Point {
+fn rotate((x, y, z): Point, i: usize) -> Point {
     let (x, y, z) = match i / 4 {
         0 => (x, y, z),
         1 => (-x, y, -z),
@@ -51,94 +58,85 @@ fn orient((x, y, z): Point, i: usize) -> Point {
     }
 }
 
-fn rectify(
-    unsolved_beacon: Point,
-    solved_beacon: Point,
-    unsolved_beacons: &[Point],
-    solved_beacons: &[Point],
-    unsolved_norms: &HashMap<Point, HashSet<isize>>,
-) -> (Point, (Vec<Point>, HashMap<Point, HashSet<isize>>)) {
-    let solved_set: HashSet<_> = solved_beacons.iter().collect();
-    for orientation in 0..24 {
-        let oriented_unsolved_beacon = orient(unsolved_beacon, orientation);
-        let displacement = sub(solved_beacon, oriented_unsolved_beacon);
-        let corrected_beacons: Vec<_> = unsolved_beacons
-            .iter()
-            .map(|&beacon| add(displacement, orient(beacon, orientation)))
-            .collect();
-        let corrected_set: HashSet<_> = corrected_beacons.iter().collect();
-        let overlap: HashSet<_> = corrected_set.intersection(&solved_set).collect();
-        if overlap.len() > 11 {
-            let corrected_norms: HashMap<_, _> = unsolved_norms
-                .iter()
-                .map(|(point, norms)| {
-                    (
-                        add(displacement, orient(*point, orientation)),
-                        norms.clone(),
-                    )
-                })
-                .collect();
-            return (
-                sub((0, 0, 0), displacement),
-                (corrected_beacons, corrected_norms),
-            );
-        }
-    }
-    unreachable!()
-}
-
-fn solve(scanners: &[Vec<Point>]) -> Vec<(Point, Vec<Point>)> {
-    let displacements: Vec<_> = scanners
+fn find_match(
+    known_fingerprints: &Fingerprints,
+    beacons: &[Point],
+    fingerprints: &[(Fingerprint, (Point, Point))],
+) -> Option<(Point, Vec<Point>)> {
+    let matching_fprints: Vec<_> = fingerprints
         .iter()
-        .map(|beacons| {
-            let mut pairs: HashMap<_, HashSet<_>> = HashMap::new();
-            for i in 0..beacons.len() {
-                for j in (i + 1)..beacons.len() {
-                    let n = norm(beacons[i], beacons[j]);
-                    pairs.entry(beacons[i]).or_default().insert(n);
-                    pairs.entry(beacons[j]).or_default().insert(n);
+        .filter(|(fprint, _)| known_fingerprints.contains_key(fprint))
+        .collect();
+
+    if matching_fprints.len() < 66 {
+        return None;
+    }
+
+    for (f, unknown_pair) in matching_fprints {
+        for known_pair in known_fingerprints.get(f).unwrap() {
+            let (k0, k1) = *known_pair;
+            let (u0, u1) = *unknown_pair;
+
+            for rotation in 0..24 {
+                if sub(k0, rotate(u0, rotation)) == sub(k1, rotate(u1, rotation)) {
+                    let translation = sub(k0, rotate(u0, rotation));
+
+                    let transformed_beacons: Vec<_> = beacons
+                        .iter()
+                        .map(|&p| add(translation, rotate(p, rotation)))
+                        .collect();
+                    return Some((translation, transformed_beacons));
                 }
             }
-            pairs
+        }
+    }
+    unreachable!();
+}
+
+fn extend_fingerprints(fingerprints: &mut Fingerprints, scanner: &[Point]) {
+    for i in 0..scanner.len() {
+        for j in (i + 1)..scanner.len() {
+            let f = fingerprint(scanner[i], scanner[j]);
+            fingerprints
+                .entry(f)
+                .or_default()
+                .push((scanner[i], scanner[j]));
+        }
+    }
+}
+
+fn solve(scanners: &[Vec<Point>]) -> (Vec<Point>, HashSet<Point>) {
+    let mut known_scanners = vec![(0, 0, 0)];
+    let mut known_beacons: HashSet<_> = scanners[0].clone().into_iter().collect();
+    let mut known_fingerprints = HashMap::new();
+    extend_fingerprints(&mut known_fingerprints, &scanners[0]);
+
+    let mut unknown_beacons: Vec<_> = scanners[1..]
+        .iter()
+        .map(|scanner| {
+            let fingerprints: Vec<_> = scanner
+                .iter()
+                .flat_map(|&a| scanner.iter().map(move |&b| (fingerprint(a, b), (a, b))))
+                .collect();
+            (scanner, fingerprints)
         })
         .collect();
 
-    let zipped: Vec<_> = scanners.iter().zip(displacements).collect();
-
-    let mut solved = vec![((0, 0, 0), (zipped[0].0.clone(), zipped[0].1.clone()))];
-    let mut unsolved: Vec<_> = zipped[1..].iter().collect();
-
-    while !unsolved.is_empty() {
-        unsolved.retain(|(unsolved_beacons, unsolved_displacements)| {
-            for (_, (solved_beacons, solved_displacements)) in &solved {
-                for unsolved_beacon in *unsolved_beacons {
-                    for solved_beacon in solved_beacons {
-                        let unsolved_norms = unsolved_displacements.get(unsolved_beacon).unwrap();
-                        let solved_norms = solved_displacements.get(solved_beacon).unwrap();
-                        let intersect: HashSet<_> =
-                            unsolved_norms.intersection(solved_norms).collect();
-                        if intersect.len() > 10 {
-                            let rectified = rectify(
-                                *unsolved_beacon,
-                                *solved_beacon,
-                                unsolved_beacons,
-                                solved_beacons,
-                                unsolved_displacements,
-                            );
-                            solved.push(rectified);
-                            return false;
-                        }
-                    }
-                }
+    while !unknown_beacons.is_empty() {
+        unknown_beacons.retain(|(beacons, fingerprints)| {
+            if let Some((scanner, transformed_beacons)) =
+                find_match(&known_fingerprints, beacons, fingerprints)
+            {
+                extend_fingerprints(&mut known_fingerprints, &transformed_beacons);
+                known_scanners.push(scanner);
+                known_beacons.extend(transformed_beacons);
+                false
+            } else {
+                true
             }
-            true
         });
     }
-
-    solved
-        .into_iter()
-        .map(|(origin, (beacons, _))| (origin, beacons))
-        .collect()
+    (known_scanners, known_beacons)
 }
 
 fn main() {
