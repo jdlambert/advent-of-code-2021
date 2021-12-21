37f6ca84a054833c1b4dcb76ada163c213dd9f79
Jacob Lambert <jacob.lambert@gmail.com>
2021-12-20 18:34:20 -0800

    Further simplify day 20

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,11 +1,12 @@
 struct Image {
     pixels: Vec<u8>,
+    height: usize,
     width: usize,
     padding: u8,
 }
 
 fn index_or_pad(image: &Image, i: Option<usize>, j: usize) -> usize {
-    if i.is_some() && i.unwrap() < image.pixels.len() / image.width && j < image.width {
+    if i.is_some() && i.unwrap() < image.height && j < image.width {
         image.pixels[i.unwrap() * image.width + j] as usize
     } else {
         image.padding as usize
@@ -17,7 +18,7 @@ fn down_num(image: &Image, prev: usize, i: usize) -> usize {
     (prev & 0b111_111) << 3 // Lose the top three bits
         | (image.padding as usize) << 2
         | (image.padding as usize) << 1
-        | corner as usize
+        | corner
 }
 
 fn right_num(image: &Image, prev: usize, i: usize, j: usize) -> usize {
@@ -25,14 +26,14 @@ fn right_num(image: &Image, prev: usize, i: usize, j: usize) -> usize {
     let middle = index_or_pad(image, i.checked_sub(1), j);
     let bottom = index_or_pad(image, Some(i), j);
     (prev & 0b011_011_011) << 1 // Lose the leftmost three bits
-        | (top as usize) << 6
-        | (middle as usize) << 3
-        | bottom as usize
+        | top << 6
+        | middle << 3
+        | bottom
 }
 
 fn step(image: &Image, code: &Vec<u8>) -> Image {
     let new_width = image.width + 2;
-    let new_height = image.pixels.len() / image.width + 2;
+    let new_height = image.height + 2;
 
     let mut new_pixels = vec![0; new_width * new_height];
     let mut row_head_score = if image.padding == 0 { 0 } else { 511 };
@@ -55,7 +56,8 @@ fn step(image: &Image, code: &Vec<u8>) -> Image {
     Image {
         pixels: new_pixels,
         padding,
-        width: image.width + 2,
+        width: new_width,
+        height: new_height,
     }
 }
 
@@ -76,9 +78,11 @@ fn main() {
     let pixels = parse(image);
 
     let width = image.lines().next().unwrap().len();
+    let height = pixels.len() / width;
     let mut image = Image {
         pixels,
         width,
+        height,
         padding: 0,
     };
     for _ in 0..2 {
